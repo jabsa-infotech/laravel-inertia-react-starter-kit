682545f51bf23fd84e7734f0d791fcd214ee76bd
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2024-09-18 20:54:28 +0530

    Navbar

---
+++ resources/js/Layouts/AuthenticatedLayoutPartials/Navbar.tsx
@@ -0,0 +1,126 @@
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import Dropdown from '@/Components/Dropdown';
+import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
+import { Bar3BottomLeft } from '@/icons/HeroIcons';
+import { User } from '@/types';
+import { Button } from '@headlessui/react';
+import { Link } from '@inertiajs/react'
+import React from 'react'
+
+interface NavbarProps {
+    user: User;
+    onToggleSidebar: () => void;
+    sidebarOpen: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ onToggleSidebar, sidebarOpen, user }) => {
+    return (
+        <>
+            <nav className="sticky top-0 z-50 border-b border-gray-200 bg-stone-300 print:hidden ">
+                <div className="px-4 sm:pr-6 lg:pr-8">
+                    <div className="flex justify-between h-16">
+                        <div className="flex">
+                            <div className="flex items-center gap-4 shrink-0">
+                                <Button onClick={onToggleSidebar} className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg ms-3 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"><Bar3BottomLeft /></Button>
+                                <Link href={route('dashboard')}>
+                                    <ApplicationLogo className="block w-auto h-6 text-gray-800 fill-current dark:text-gray-200" />
+                                </Link>
+                            </div>
+
+                            <div className="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+
+                            </div>
+                        </div>
+
+                        <div className="hidden sm:flex sm:items-center sm:ms-6">
+                            <div className="relative ms-3">
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out border border-transparent rounded-md bg-stone-300 hover:text-gray-700 focus:outline-none"
+                                            >
+                                                {user.name}
+
+                                                <svg
+                                                    className="ms-2 -me-0.5 h-4 w-4"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 20 20"
+                                                    fill="currentColor"
+                                                >
+                                                    <path
+                                                        fillRule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clipRule="evenodd"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
+                                        <Dropdown.Link href={route('logout')} method="post" as="button">
+                                            Log Out
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center -me-2 sm:hidden">
+                            <button
+                                onClick={onToggleSidebar}
+                                className="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400"
+                            >
+                                <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                    <path
+                                        className={sidebarOpen ? 'inline-flex' : 'hidden'}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M4 6h16M4 12h16M4 18h16"
+                                    />
+                                    <path
+                                        className={sidebarOpen ? 'inline-flex' : 'hidden'}
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={(sidebarOpen ? 'block' : 'hidden') + ' sm:hidden'}>
+                    <div className="pt-2 pb-3 space-y-1">
+                        <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
+                            Dashboard
+                        </ResponsiveNavLink>
+                    </div>
+
+                    <div className="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
+                        <div className="px-4">
+                            <div className="text-base font-medium text-gray-800 dark:text-gray-200">
+                                {user.name}
+                            </div>
+                            <div className="text-sm font-medium text-gray-500">{user.email}</div>
+                        </div>
+
+                        <div className="mt-3 space-y-1">
+                            <ResponsiveNavLink href={route('profile.edit')}>Profile</ResponsiveNavLink>
+                            <ResponsiveNavLink method="post" href={route('logout')} as="button">
+                                Log Out
+                            </ResponsiveNavLink>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
